79e4726a970657f016e72cd45d7d3a07a5ded93b
Anwesh43 <anweshthecool0@gmail.com>
2020-08-17 23:11:07 +0530

    adding Renderer class

--- stepjumpstickview/src/main/java/com/anwesh/uiprojects/stepjumpstickview/StepJumpStickView.kt
+++ stepjumpstickview/src/main/java/com/anwesh/uiprojects/stepjumpstickview/StepJumpStickView.kt
@@ -183,4 +183,27 @@ class StepJumpStickView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : StepJumpStickView) {
+
+        private val animator : Animator = Animator(view)
+        private val sjs : StepJumpStick = StepJumpStick(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sjs.draw(canvas, paint)
+            animator.animate {
+                sjs.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sjs.startUpdating {
+                animator.start()
+            }
+        }
+    }
  }
