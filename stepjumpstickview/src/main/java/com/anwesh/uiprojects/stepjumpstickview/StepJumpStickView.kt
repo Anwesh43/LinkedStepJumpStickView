068d5bb705e6aff160316e4b8cc5094315d30ce8
Anwesh43 <anweshthecool0@gmail.com>
2020-08-17 20:21:57 +0530

    adding Cavas extension method

--- stepjumpstickview/src/main/java/com/anwesh/uiprojects/stepjumpstickview/StepJumpStickView.kt
+++ stepjumpstickview/src/main/java/com/anwesh/uiprojects/stepjumpstickview/StepJumpStickView.kt
@@ -16,7 +16,7 @@ import android.app.Activity
 val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#F44336", "#009688", "#03A9F4")
 val parts : Int = 4
 val scGap : Float = 0.02f / parts
-val sizeFactor : Float = 3.8f
+val sizeFactor : Float = 6.8f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 
@@ -24,3 +24,35 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawStepJumpStick(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc11 : Float = sc1.divideScale(0, 2)
+    val sc12 : Float = sc1.divideScale(1, 2)
+    val sc21 : Float = sc2.divideScale(0, 2)
+    val sc22 : Float = sc2.divideScale(1, 2)
+    val sf1 : Float = sc12.sinify()
+    val sf2 : Float = sc22.sinify()
+    save()
+    translate(w / 2, h)
+    for (j in 0..1) {
+        val r : Float = size * (sc1 - sc4)
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-w / 2 * (1 - sf1) * (1 - sf2), -(h / - size) * (sc2 + sc3))
+        drawRect(RectF(-r, -r, r, r), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSJSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawStepJumpStick(scale, w, h, paint)
+}
